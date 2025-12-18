45e3d036aacee63512983bc84c3c484eeed1fc58
Orlovchikk <svetl.orlovaa@gmail.com>
2025-12-18 10:59:32 +0100

    still kinda docker compose 2

--- src/swompi/config.py
+++ src/swompi/config.py
@@ -1,13 +1,17 @@
 from pydantic_settings import BaseSettings
 
 class AppConfig(BaseSettings):
-    DATABASE_URL: str
-    S3_ENDPOINT_URL: str
+    POSTGRES_DB: str = "postgres"
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_HOSTNAME: str = "database"
+    S3_ENDPOINT_URL: str = "http://127.0.0.1:3900"
     S3_ACCESS_KEY: str
     S3_SECRET_KEY: str
-    S3_DEFAULT_REGION: str
+    S3_DEFAULT_REGION: str = "garage"
     GITHUB_WEBHOOK_SECRET: str
     TELEGRAM_BOT_TOKEN: str
 
     class Config:
-        env_file = ".env"
+        env_file = ("../../garage/garage.env", ".env")
+        env_file_encoding = 'utf-8'
